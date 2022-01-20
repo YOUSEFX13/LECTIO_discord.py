9524508fb6d5b61d30addaabfd21ef39ea1582ea
Yusuf K <yusufkaya@outlook.dk>
2022-01-20 15:08:59 +0100

    Co-authored-by: TheTimeShare <TheTimeShare@users.noreply.github.com>

---
+++ debug.py
@@ -0,0 +1,16 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+
+client.run(TOKEN)
--- explain.py
+++ explain.py
@@ -27,4 +27,47 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+# join message
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!')
+
+# join message end
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '$Mike':
+        response = 'Check'+' ' + (message.author.mention)
+        await message.channel.send(response)
+
+
+@client.evoice_clients
+async def on_member(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '$Github':
+        response = 'Our Github page:
+
+
+@client.event
+async def on_member_leave(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'{member.name},Left the Discord server!')
+
+
 client.run(TOKEN)
