0e5145bb07a66d05ab4a1e7ab94ce4fc939eceed
Yusuf K <yusufkaya@outlook.dk>
2022-01-22 03:17:17 +0100

    follow up of the commit before

--- debug.py
+++ debug.py
@@ -1,15 +1,15 @@
 # debugbot.py
 import os
-import discord
+import nextcord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
+intents = nextcord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
+client = nextcord.Client(intents=intents)
 
 
 client.run(TOKEN)
