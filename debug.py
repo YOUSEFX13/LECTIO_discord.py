19cfa404e0cc162016c1b20928adcce4e6d45f6e
Yusuf K <yusufkaya@outlook.dk>
2022-01-22 23:56:01 +0100

    FINALLY DONE MUSIC (wip)

--- debug.py
+++ debug.py
@@ -1,7 +1,12 @@
 # debugbot.py
-import os
 import nextcord
+import os
+# load our local env so we dont have the token in public
 from dotenv import load_dotenv
+from nextcord.utils import get
+from nextcord import FFmpegPCMAudio
+from nextcord import TextChannel
+from youtube_dl import YoutubeDL
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,6 +15,58 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = nextcord.Intents.default()
 intents.members = True
 client = nextcord.Client(intents=intents)
+
+
+def updatevar():
+    global source
+    source = (open('Config.txt').read())
+    global mylist
+    mylist = source.split("=")[1]
+    global prefix
+    prefix = str(mylist)
+    print('Current prefix is = '+prefix)
+
+
+updatevar()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(prefix+'play'):  # PLAY COMMAND
+
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        voice = get(client.voice_clients, guild=message.guild)
+        global url
+        url = message.content.split(" ")[1]
+
+        if not voice.is_playing():
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+            URL = info['url']
+            voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+            voice.is_playing()
+            await message.channel.send(':notes: Bot is playing '+url+' enjoy.')
+
+        else:
+            await message.channel.send("Bot is already playing some other music")
+            return
+
+    if message.content == (prefix+'join'):  # JOIN COMMAND
+        channel = message.author.voice.channel
+        voice = get(client.voice_clients, guild=message.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+
+    if message.content == (prefix+'leave'):  # leave command
+        voice = get(client.voice_clients, guild=message.guild)
+        await voice.disconnect() and message.channel.send('left the channel')
 
 
 client.run(TOKEN)
--- explain.py
+++ explain.py
@@ -1,8 +1,12 @@
 # bot.py
-import os
-
 import nextcord
+import os
+# load our local env so we dont have the token in public
 from dotenv import load_dotenv
+from nextcord.utils import get
+from nextcord import FFmpegPCMAudio
+from nextcord import TextChannel
+from youtube_dl import YoutubeDL
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -63,16 +67,53 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == prefix+'Mike':
+    if message.content == prefix+'Mike':  # mike
         response = 'Check'+' ' + (message.author.mention)
         await message.channel.send(response)
 
     if message.author == client.user:
         return
 
-    if message.content == prefix+'github':
+    if message.content == prefix+'github':  # github command
         response = 'Our Github page: https://github.com/YOUSEFX13/discord.py'
         await message.channel.send(response)
+
+    if message.content == prefix+'help':  # github command
+        response = 'WIP'
+        await message.channel.send(response)
+
+    if message.content.startswith(prefix+'play'):  # PLAY COMMAND
+
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        voice = get(client.voice_clients, guild=message.guild)
+        global url
+        url = message.content.split(" ")[1]
+
+        if not voice.is_playing():
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+            URL = info['url']
+            voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+            voice.is_playing()
+            await message.channel.send(':notes: Bot is playing '+url+' enjoy.')
+
+        else:
+            await message.channel.send("Bot is already playing some other music")
+            return
+
+    if message.content == (prefix+'join'):  # JOIN COMMAND
+        channel = message.author.voice.channel
+        voice = get(client.voice_clients, guild=message.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+
+    if message.content == (prefix+'leave'):  # leave command
+        voice = get(client.voice_clients, guild=message.guild)
+        await voice.disconnect() and message.channel.send('left the channel')
 
 
 @client.event  # leave message
