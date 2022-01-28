73a44fa4f90dff657c1f6edc357d622c77d46433
Yusuf K <yusufkaya@outlook.dk>
2022-01-28 03:47:00 +0100

    added so when ya write !skema u get schedule for the day

--- debug.py
+++ debug.py
@@ -9,6 +9,7 @@ from uritemplate import variables
 from youtube_dl import YoutubeDL
 from src.lectio import Lectio
 import json
+import datetime
 
 
 # load our local env so we dont have the token in public
@@ -42,7 +43,43 @@ def updatevar():
 updatevar()
 
 
-# lec = Lectio(LECNAME, LECPASS, SCHOOLID)
+def lectiotime():
+    a = datetime.datetime.now()
+
+    global curDay
+    curDay = a.strftime("%d")
+    if curDay.startswith('0'):
+        curDay = curDay.split('0')[1]
+        pass
+
+    else:
+        pass
+
+    global curMonth
+    curMonth = a.strftime("%m")
+    if curMonth.startswith('0'):
+
+        curMonth = curMonth.split('0')[1]
+        pass
+
+    else:
+        pass
+
+    global curYear
+    curYear = a.strftime("%Y")
+
+    global curDate
+    curDate = (curDay+'/'+curMonth+'-'+curYear)
+
+    global DANUMBA
+    DANUMBA = {}
+    global thenumba
+    thenumba = {}
+
+
+lectiotime()
+
+lec = Lectio(LECNAME, LECPASS, SCHOOLID)
 
 
 @client.event
@@ -50,42 +87,64 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(prefix+'skema'):  # mike
+    if message.content == (prefix+'skema'):  # mike
 
-        numberint = int(message.content.split(" ")[1])
+        skema = (str(lec.getSchedule()))
 
-        lec = Lectio(LECNAME, LECPASS, SCHOOLID)
-        skema = (lec.getSchedule())
-        svar = str(skema)
-        anan = svar.replace("\'", "\"")
+        anan = skema.replace("\'", "\"")
+
         x = json.loads(anan)
-        y = x[numberint]
 
-        time = y['Time']
-        team = y['Team']
-        teacher = y['Teacher']
-        room = y['Room']
+        aa = (int(len(x)))
 
-        embed = nextcord.Embed(title="Link To Lectio", url="https://www.lectio.dk",
-                               description="Hello! Here is your schedule for the day :D ", color=0x109319)
+        for ad in range(aa):
+            global yy
 
-        embed.set_author(name="DEVSEVBOT", url="https://www.youtube.com/watch?v=989-7xsRLR4",
-                         icon_url="https://pbs.twimg.com/profile_images/1327036716226646017/ZuaMDdtm_400x400.jpg")
+            yy = x[int(ad)]
+            ff = str(yy['Time'].split(curYear)[0])
+            kk = ff.replace("-", "-"+curYear)
 
-        embed.set_thumbnail(url="https://i.imgur.com/55EaVfW.png")
-        embed.add_field(name="Time",
-                        value=time, inline=False)
-        embed.add_field(name="Team",
-                        value=team, inline=False)
-        embed.add_field(name="Teacher",
-                        value=teacher, inline=True)
-        embed.add_field(name="room",
-                        value=room, inline=True)
+            DANUMBA[str(kk)] = str(ad)
 
-        embed.set_footer(
-            text="Made by ๖ۣۜℜ𝒾bar𝒾⚔#9594 & жар#9179")
+            gg = DANUMBA.get(str(curDate), 'del')
+            u = [(gg, {gg: gg})]
 
-        await message.channel.send(embed=embed)
+            thenumba.update(u)
+
+        if 'del' in thenumba:
+            thenumba.pop('del')
+        else:
+            pass
+        for l in thenumba:
+            numberint = int(l)
+            y = x[numberint]
+
+            time = y['Time']
+            team = y['Team']
+            teacher = y['Teacher']
+            room = y['Room']
+            urlid = y['Id']
+
+            embed = nextcord.Embed(title="Link To Modul", url="https://www.lectio.dk/lectio/"+SCHOOLID+"/aktivitet/aktivitetforside2.aspx?absid="+urlid,
+                                   description="Hello! Here is your schedule for the day :D ", color=0x109319)
+
+            embed.set_author(name="DEVSEVBOT", url="https://www.youtube.com/watch?v=989-7xsRLR4",
+                             icon_url="https://pbs.twimg.com/profile_images/1327036716226646017/ZuaMDdtm_400x400.jpg")
+
+            embed.set_thumbnail(url="https://i.imgur.com/55EaVfW.png")
+            embed.add_field(name="Time",
+                            value=time, inline=False)
+            embed.add_field(name="Team",
+                            value=team, inline=False)
+            embed.add_field(name="Teacher",
+                            value=teacher, inline=True)
+            embed.add_field(name="Room",
+                            value=room, inline=True)
+
+            embed.set_footer(
+                text="Made by ๖ۣۜℜ𝒾bar𝒾⚔#9594 & жар#9179")
+
+            await message.channel.send(embed=embed)
 
 
 client.run(TOKEN)
--- test.py
+++ test.py
@@ -1,18 +1,86 @@
 from src.lectio import Lectio
+import os
 import json
+import datetime
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+LECNAME = os.getenv('Lectio_name')
+LECPASS = os.getenv('Lectio_pass')
+SCHOOLID = os.getenv('Lectio_ID')
+lec = Lectio(LECNAME, LECPASS, SCHOOLID)
 
 
-lec = Lectio("21Epsilon27", "Kaya7601", "523")
+def lectiotime():
+    a = datetime.datetime.now()
+
+    global curDay
+    curDay = a.strftime("%d")
+    if curDay.startswith('0'):
+        curDay = curDay.split('0')[1]
+        pass
+
+    else:
+        pass
+
+    global curMonth
+    curMonth = a.strftime("%m")
+    if curMonth.startswith('0'):
+
+        curMonth = curMonth.split('0')[1]
+        pass
+
+    else:
+        pass
+
+    global curYear
+    curYear = a.strftime("%Y")
+
+    global curDate
+    curDate = (curDay+'/'+curMonth+'-'+curYear)
+
+    global DANUMBA
+    DANUMBA = {}
+    global thenumba
+    thenumba = {}
+
+
+lectiotime()
+
+
 # Print out your exercises
 
 
-ohye = (str(lec.getSchedule()))
+skema = (str(lec.getSchedule()))
 
-anan = ohye.replace("\'", "\"")
+anan = skema.replace("\'", "\"")
 
 x = json.loads(anan)
 
-y = x[0]
-z = y.keys()
 
-print(y['Team'])
+aa = (int(len(x)))
+testm = str(1)
+
+
+for ad in range(aa):
+    global yy
+    yy = x[int(ad)]
+    ff = str(yy['Time'].split(curYear)[0])
+    kk = ff.replace("-", "-"+curYear)
+
+    DANUMBA[str(kk)] = str(ad)
+
+    gg = DANUMBA.get(str(curDate), 'del')
+    u = [(gg, {gg: gg})]
+
+    thenumba.update(u)
+
+    if 'del' in thenumba:
+        thenumba.pop('del')
+    else:
+        pass
+
+
+for l in thenumba:
+    print(l)
